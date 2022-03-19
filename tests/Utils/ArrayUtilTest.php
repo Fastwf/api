19c76b076d614d1d5fbb706092a853da65cf1daa
David BELBEZE <david.belbeze@gmail.com>
2022-03-19 21:09:00 +0100

    Update ArrayUtil
    Use get and getSafe with array reference
    Add merge method

--- tests/Utils/ArrayUtilTest.php
+++ tests/Utils/ArrayUtilTest.php
@@ -7,36 +7,77 @@ use PHPUnit\Framework\TestCase;
 use Fastwf\Api\Utils\ArrayUtil;
 use Fastwf\Api\Exceptions\KeyError;
 
-class ArrayUtilTest extends TestCase {
+class ArrayUtilTest extends TestCase
+{
+
+    private $array = [
+        'key' => 'value'
+    ];
 
     /**
      * @covers Fastwf\Api\Utils\ArrayUtil
      */
-    public function testGet() {
-        $this->assertEquals('foo', ArrayUtil::get(['bar' => 'foo'], 'bar'));
+    public function testGet()
+    {
+        $this->assertEquals('value', ArrayUtil::get($this->array, 'key'));
     }
 
     /**
      * @covers Fastwf\Api\Utils\ArrayUtil
      */
-    public function testGetError() {
+    public function testGetException()
+    {
         $this->expectException(KeyError::class);
-
-        ArrayUtil::get(['bar' => 'foo'], 'foo');
+        
+        ArrayUtil::get($this->array, 'not-found');
     }
 
     /**
      * @covers Fastwf\Api\Utils\ArrayUtil
      */
-    public function testGetSafe() {
-        $this->assertEquals('foo', ArrayUtil::getSafe(['bar' => 'foo'], 'bar'));
+    public function testGetSafe()
+    {
+        $this->assertEquals('value', ArrayUtil::getSafe($this->array, 'key'));
     }
 
     /**
      * @covers Fastwf\Api\Utils\ArrayUtil
      */
-    public function testGetSafeDefault() {
-        $this->assertEquals('bar', ArrayUtil::getSafe(['bar' => 'foo'], 'foo', 'bar'));
+    public function testGetSafeDefault()
+    {
+        $this->assertEquals('other', ArrayUtil::getSafe($this->array, 'not-found', 'other'));
+    }
+
+    /**
+     * @covers Fastwf\Api\Utils\ArrayUtil
+     */
+    public function testMergeAll()
+    {
+        $from = ['a' => 'A', 'b' => 'B'];
+        $to = ['c' => 'C'];
+
+        ArrayUtil::merge($from, $to);
+
+        $this->assertEquals(
+            ['a' => 'A', 'b' => 'B', 'c' => 'C'],
+            $to,
+        );
+    }
+
+    /**
+     * @covers Fastwf\Api\Utils\ArrayUtil
+     */
+    public function testMergeSubSet()
+    {
+        $from = ['a' => 'A', 'b' => 'B'];
+        $to = ['c' => 'C'];
+
+        ArrayUtil::merge($from, $to, ['b', 'd']);
+
+        $this->assertEquals(
+            ['b' => 'B', 'c' => 'C'],
+            $to,
+        );
     }
 
 }
